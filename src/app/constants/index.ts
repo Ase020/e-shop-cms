411772ab99e511da37ac223b33590a91351487a7
felixNyalenda <felixasejunior@gmail.com>
2023-12-05 12:51:49 +0300

    Footer build

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,20 +1,24 @@
 export const inclusions = [
   {
+    id: 1,
     title: 'Free Shipping',
     description: 'Free shipping for order above $150',
     icon: '/assets/icons/shipping.svg',
   },
   {
+    id: 2,
     title: 'Money Guarantee',
     description: 'Within 30 days for an exchange',
     icon: '/assets/icons/dollar.svg',
   },
   {
+    id: 3,
     title: 'Online Support',
     description: '24 hours a day, 7 days a week',
     icon: '/assets/icons/support.svg',
   },
   {
+    id: 4,
     title: 'Flexible Payment',
     description: 'Pay with multiple credit cards',
     icon: '/assets/icons/payment.svg',
@@ -23,21 +27,25 @@ export const inclusions = [
 
 export const profileNavItems = [
   {
+    id: 1,
     title: 'Personal Information',
     url: '/account',
     icon: '/assets/icons/user.svg',
   },
   {
+    id: 2,
     title: 'My Purchases',
     url: '/account/purchases',
     icon: '/assets/icons/purchases.svg',
   },
   {
+    id: 3,
     title: 'My Orders',
     url: '/account/orders',
     icon: '/assets/icons/orders.svg',
   },
   {
+    id: 4,
     title: 'Logout',
     url: '/logout',
     icon: '/assets/icons/logout.svg',
--- src/payload/fields/link.ts
+++ src/payload/fields/link.ts
@@ -116,6 +116,12 @@ const link: LinkType = ({ appearances, disableLabel = false, overrides = {} } = 
             width: '50%',
           },
         },
+        {
+          name: 'icon',
+          label: 'Label',
+          type: 'upload',
+          relationTo: 'media',
+        },
       ],
     })
   } else {
